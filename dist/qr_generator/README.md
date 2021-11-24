e0e3793118c40a1528373344dcab83350ce8099c
vukilis <vuklekic22@gmail.com>
2021-11-24 19:27:59 +0100

    commit

---
+++ dist/qr_generator/README.md
@@ -0,0 +1,23 @@
+## Screenshot
+![Image of Yaktocat](https://raw.githubusercontent.com/vukilis/QR_Generator/main/images/APP.png)
+
+## Installation
+
+Use the [git](https://git-scm.com/) to download QR Generator.
+
+```bash
+git clone https://github.com/vukilis/QR_Generator
+```
+## How to use
+```bash
+python qr_generator.py
+```
+**OR**
+
+**``
+Simple just open shortcut "QR Generator" in "QR Generator" directory
+``**
+
+## About
+
+_I made this app as a project in purpose to learn python and kivy. This is app that I built to generate QR code._
---
+++ dist/qr_generator/qr_generator.py
@@ -0,0 +1,69 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.popup import Popup
+from kivy.core.window import Window
+from kivy.lang import Builder
+import qrcode
+import webbrowser
+from kivy.clock import Clock
+
+Window.size = (800, 600)
+Window.minimum_width, Window.minimum_height = Window.size
+
+class Progress(Popup):
+    
+    def __init__(self, **kwargs):
+        super(Progress, self).__init__(**kwargs)
+        Clock.schedule_once(self.dismiss_popup, 2)
+
+    def dismiss_popup(self, dt):
+        self.dismiss()
+
+class MyLayout(ScreenManager):
+    def generate_qr_code(self, args):
+        if self.ids.link_text.text != "" and self.ids.img_name.text != "":
+            qr = qrcode.QRCode(version=1,box_size=15,border=5)
+            qr.add_data(self.ids.link_text.text)
+            qr.make(fit=True)
+            img = qr.make_image(fill='black', back_color='white')
+            img.save(f"{self.ids.img_name.text}.png")
+            popup = Progress()
+            popup.open()
+        print("Generate Button is pressed")
+        
+    def view_qr(self, app):
+        if self.ids.img_name.text != "":
+            self.ids.qr_code_.source = f"{self.ids.img_name.text}.png"
+        else:
+            self.ids.qr_code_.source = "images/preview.png"
+    
+    def show_first_page(self, root):
+        root.current = "first"
+            
+    def show_second_page(self, root):
+        root.current = "second"
+    
+    data = {
+        'Portfolio': 'account',
+        'Github': 'github'
+    }
+    
+class QR_Generator(MDApp):  
+    def callback(self, instance):
+        if instance.icon == 'account':
+            webbrowser.open("https://vuklekic.herokuapp.com/")
+        if instance.icon == 'github':
+            webbrowser.open("https://github.com/vukilis")
+    
+    Builder.load_file('my.kv', encoding='utf8')   
+    def build(self):
+        self.icon = "images/logo.png"
+        self.theme_cls.theme_style = "Dark"
+        self.primary_dark_hue="Yellow"
+        self.theme_cls.primary_palette = "DeepOrange"
+        self.theme_cls.primary_hue = "400"
+
+        return MyLayout()
+    
+QR_Generator().run()
